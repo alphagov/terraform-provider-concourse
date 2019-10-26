fc81998d6d4dc54773d93ebf71a6c164be8b600c
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-10-26 17:21:13 +0100

    Increase Concourse wait time
    GitHub actions is slow to boot Concourse
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- integration/setup_test.go
+++ integration/setup_test.go
@@ -72,7 +72,7 @@ func SetupTest() {
 
 		fmt.Println("Concourse is ready")
 		return nil
-	}, "15s", "3s").ShouldNot(HaveOccurred())
+	}, "60s", "3s").ShouldNot(HaveOccurred())
 }
 
 func TeardownTest() {
